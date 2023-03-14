6256e0587433e9e3e8c815c1caf7a88449db352f
Michael Strobel <lidstromfan29@gmail.com>
2023-03-14 08:17:22 -0700

    Move subset generation to vaex.

--- bin/GNPS2_Subset_Generator.py
+++ bin/GNPS2_Subset_Generator.py
@@ -2,6 +2,10 @@ import re
 import pandas as pd
 import argparse
 import os
+import vaex
+
+def slice_parquet(file_path:str, spectrum_ids:list, batch_size:int=100):
+    pf = vaex.open(file_path)
 
 def Bruker_Fragmentation_Prediction(summary_path:str, parquet_path:str):
     """This function follows the cleaning in 3DMolMS applied to Bruker qtof instruments.
@@ -15,37 +19,44 @@ def Bruker_Fragmentation_Prediction(summary_path:str, parquet_path:str):
     parquet_as_df = pd.read_parquet(parquet_path)
     
     allowed_atoms = ['C', 'H', 'O', 'N', 'F', 'S', 'Cl', 'P', 'B', 'Br', 'I']
-    starting_size = len(reduced_df)
-    print("Starting Size:", starting_size)
+
     # Remove structure-less entries. select instrument = qTof and Adduct in ['M+H','M-H']
     reduced_df = reduced_df[~ (reduced_df['Smiles'].isna() ) & (reduced_df['msMassAnalyzer'] == 'qtof') & ((reduced_df['Adduct'] == 'M+H') | (reduced_df['Adduct'] == 'M-H'))].copy(deep=True)
-    print("Lost {} structures when requiring smiles, msMassAnalyzer == 'qtof', and M+H/M-H".format(starting_size - len(reduced_df)))
+
     # Remove all entires with atoms not in ['C', 'H', 'O', 'N', 'F', 'S', 'Cl', 'P', 'B', 'Br', 'I']
     reduced_df['Smiles_letters_only'] = reduced_df['Smiles'].apply(lambda x: "".join(re.findall("[a-zA-Z]+", x)))
     reduced_df['Smiles_cleaned'] = reduced_df['Smiles_letters_only'].apply(lambda x: "".join(re.findall("^[" + "|".join(allowed_atoms) + "]+$", x)))
     reduced_df = reduced_df[reduced_df['Smiles_cleaned'] != ""]
     reduced_df.drop(['Smiles_letters_only','Smiles_cleaned'], inplace=True, axis=1)
-    print(reduced_df.msManufacturer.value_counts())
+
     reduced_df = reduced_df[reduced_df.msManufacturer == 'Bruker Daltonics']  
-    print("Ending Size:", len(reduced_df))
+
+
     
-    reduced_df.to_csv('./summary/Bruker_Fragmentation_Prediction.csv', index=False)
-       
-    parquet_as_df = parquet_as_df.loc[[x in list(reduced_df.spectrum_id) for x in parquet_as_df.spectrum_id]]
-    parquet_as_df.to_parquet('./spectra/Bruker_Fragmentation_Prediction.parquet')
+    id_list = list(reduced_df.spectrum_id )
+    del reduced_df
+    
+    parquet_as_df = vaex.open(parquet_path)
+    parquet_as_df = parquet_as_df[parquet_as_df.spectrum_id.isin(id_list)]
+    parquet_as_df.export_parquet('./spectra/MH_MNA_Translation.parquet')
     
 def MH_MNA_Translation(summary_path:str, parquet_path:str):
+
     reduced_df = pd.read_csv(summary_path)
-    parquet_as_df = pd.read_parquet(parquet_path)
+
     reduced_df = reduced_df.loc[reduced_df.msManufacturer == "Bruker Daltonics"]
     reduced_df = reduced_df.loc[reduced_df.msMassAnalyzer == 'orbitrap']
     reduced_df = reduced_df.loc[~reduced_df.Smiles.isna()]
     reduced_df = reduced_df.loc[(reduced_df.Adduct == 'M+H') | (reduced_df.Adduct == 'M+NA')]
-    reduced_df.to_csv('./summary/MH_MNA_Translation.csv', index=False)
-       
-    parquet_as_df = parquet_as_df.loc[[x in list(reduced_df.spectrum_id )for x in parquet_as_df.spectrum_id]]
-    parquet_as_df.to_parquet('./spectra/MH_MNA_Translation.parquet')
 
+    reduced_df.to_csv('./summary/MH_MNA_Translation.csv', index=False)
+    
+    id_list = list(reduced_df.spectrum_id )
+    del reduced_df
+    
+    parquet_as_df = vaex.open(parquet_path)
+    parquet_as_df = parquet_as_df[parquet_as_df.spectrum_id.isin(id_list)]
+    parquet_as_df.export_parquet('./spectra/MH_MNA_Translation.parquet')
     
 def main():
     subsets = ['Bruker_Fragmentation_Prediction','MH_MNA_Translation','GNPS_default']
