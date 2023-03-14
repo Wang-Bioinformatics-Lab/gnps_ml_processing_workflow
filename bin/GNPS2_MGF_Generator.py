abfd0f35bf477e4f5ad9f73890ec03a4c87aef6c
Michael Strobel <lidstromfan29@gmail.com>
2023-03-14 10:18:55 -0700

    Migrate MGF Generator to vaex.

--- bin/GNPS2_MGF_Generator.py
+++ bin/GNPS2_MGF_Generator.py
@@ -4,15 +4,18 @@ import pandas as pd
 from tqdm import tqdm
 from joblib import Parallel, delayed  
 from glob import glob
+import vaex
 
 def generate_mgf(parquet_file):
     name = parquet_file[:-8]
-    df = pd.read_parquet(parquet_file)
+    # df = pd.read_parquet(parquet_file)
+    df = vaex.open(parquet_file)
     to_mgf = []
     spectrum_ids = df.spectrum_id.unique()
     for i, spectrum_id in enumerate(spectrum_ids):
-        spectra = df.loc[df.spectrum_id == spectrum_id]
-        to_mgf.append({'m/z array':list(spectra.mz), 'intensity array':list(spectra.i), 'params':{'SCAN': i, 'TITLE':spectrum_id,'prec_mz':spectra.prec_mz.iloc[0][0]}})
+        # spectra = df.loc[df.spectrum_id == spectrum_id]
+        spectra = df[df.spectrum_id == spectrum_id]
+        to_mgf.append({'m/z array':spectra.mz.values, 'intensity array':spectra.i.values, 'params':{'SCAN': i, 'TITLE':spectrum_id,'prec_mz':spectra.prec_mz.values[0][0]}})
     pyteomics.mgf.write(to_mgf, output=name+'.mgf',write_charges=False, write_ions=False)
     
 def main():
