d9c418a25d1926c72ee7d98d8ac7c2d120364d86
Michael Strobel <lidstromfan29@gmail.com>
2023-03-27 14:08:41 -0700

    Add structural modification tasks.

--- bin/GNPS2_Subset_Generator.py
+++ bin/GNPS2_Subset_Generator.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import vaex
 import pickle
+from utils import build_tanimoto_similarity_list_precomputed
 
     
 def Bruker_Fragmentation_Prediction(summary_path:str, parquet_path:str):
@@ -99,6 +100,14 @@ def Orbitrap_Fragmentation_Prediction(summary_path:str, parquet_path:str):
     parquet_as_df.export_parquet('./spectra/Orbitrap_Fragmentation_Prediction.parquet')
 
 def Thermo_Bruker_Translation(summary_path:str, parquet_path:str):
+    """This function generates a csv, parquet file, and pairs list that contains the spectra of the same compounds from Thermo and Bruker instruments.
+    More specifically, we include Q Exactive Instruments from Thermo Fisher and maXis impact instruments from Bruker Daltonics.
+    In addition, we require that the adducts are identical. There are no conditions on the collision energy.
+
+    Args:
+        summary_path (str): _description_
+        parquet_path (str): _description_
+    """
     df = pd.read_csv(summary_path)
     df = df.loc[~df.Smiles.isna()]
     df = df.loc[df.Adduct.isin(['M+H','M-H'])]
@@ -128,8 +137,75 @@ def Thermo_Bruker_Translation(summary_path:str, parquet_path:str):
     parquet_as_df = parquet_as_df[parquet_as_df.spectrum_id.isin(id_list)]
     parquet_as_df.export_parquet('./spectra/Thermo_Bruker_Translation.parquet')
     
+def Structural_Modification(summary_path:str, parquet_path:str):
+    # This dataset: Struct1 + Spec1 + struct2 -> spec2
+    
+    if not os.path.isdir('./spectra/Structural_Modification'):
+        os.makedirs('./spectra/Structural_Modification', exist_ok=True)
+    if not os.path.isdir('./summary/Structural_Modification'):
+        os.makedirs('./summary/Structural_Modification', exist_ok=True)
+    if not os.path.isdir('./util/Structural_Modification'):
+        os.makedirs('./util/Structural_Modification', exist_ok=True)
+    
+    df = pd.read_csv(summary_path)
+    df = df.loc[~df.Smiles.isna()]
+    
+    parquet_as_df = vaex.open(parquet_path)
+        
+    # Orbitrap Portion
+    positive = df.loc[(df.Adduct == 'M+H') & (df.msMassAnalyzer == 'orbitrap') & (df.GNPS_Inst == 'orbitrap')]
+    negative = df.loc[(df.Adduct == 'M-H') & (df.msMassAnalyzer == 'orbitrap') & (df.GNPS_Inst == 'orbitrap')]
+    # Remove entries with duplicate SMILES
+    positive = positive.drop_duplicates(subset=['Smiles'])
+    negative = negative.drop_duplicates(subset=['Smiles'])
+    # Calculate structural similarities
+    positive_sim = build_tanimoto_similarity_list_precomputed(positive, similarity_threshold = 0.70)
+    negative_sim = build_tanimoto_similarity_list_precomputed(negative, similarity_threshold = 0.70)
+    # Remove entires not included in positive_sim
+    positive_ids = pd.concat((positive_sim.spectrumid1,positive_sim.spectrumid2)).unique()
+    negative_ids = pd.concat((negative_sim.spectrumid1,negative_sim.spectrumid2)).unique()
+    positive = positive[positive.spectrum_id.isin(positive_ids)]
+    negative = negative[negative.spectrum_id.isin(negative_ids)]
+    # Save to csv
+    positive.to_csv('./summary/Structural_Modification/orbitrap_positive.csv', index=False)
+    negative.to_csv('./summary/Structural_Modification/orbitrap_negative.csv', index=False)
+    # Save to parquet
+    parquet_as_df[parquet_as_df.spectrum_id.isin(positive_ids)].export_parquet('./spectra/Structural_Modification/orbitrap_positive.parquet')
+    parquet_as_df[parquet_as_df.spectrum_id.isin(negative_ids)].export_parquet('./spectra/Structural_Modification/orbitrap_negative.parquet')
+    
+    # Save Similarities to utils
+    positive_sim.to_csv('./util/Structural_Modification/orbitrap_positive_sim.csv', index=False)
+    negative_sim.to_csv('./util/Structural_Modification/orbitrap_negative_sim.csv', index=False)
+    
+    # qtof Portion
+    positive = df.loc[(df.Adduct == 'M+H') & (df.msMassAnalyzer == 'qtof') & (df.GNPS_Inst == 'qtof')]
+    negative = df.loc[(df.Adduct == 'M-H') & (df.msMassAnalyzer == 'qtof') & (df.GNPS_Inst == 'qtof')]
+    # Remove entries with duplicate SMILES
+    positive = positive.drop_duplicates(subset=['Smiles'])
+    negative = negative.drop_duplicates(subset=['Smiles'])
+    # Calculate structural similarities
+    positive_sim = build_tanimoto_similarity_list_precomputed(positive, similarity_threshold = 0.70)
+    negative_sim = build_tanimoto_similarity_list_precomputed(negative, similarity_threshold = 0.70)
+    # Remove entires not included in positive_sim
+    positive_ids = pd.concat((positive_sim.spectrumid1,positive_sim.spectrumid2)).unique()
+    negative_ids = pd.concat((negative_sim.spectrumid1,negative_sim.spectrumid2)).unique()
+    positive = positive[positive.spectrum_id.isin(positive_ids)]
+    negative = negative[negative.spectrum_id.isin(negative_ids)]
+    # Save to csv
+    positive.to_csv('./summary/Structural_Modification/qtof_positive.csv', index=False)
+    negative.to_csv('./summary/Structural_Modification/qtof_negative.csv', index=False)
+    # Save to parquet
+    parquet_as_df[parquet_as_df.spectrum_id.isin(positive_ids)].export_parquet('./spectra/Structural_Modification/qtof_positive.parquet')
+    parquet_as_df[parquet_as_df.spectrum_id.isin(negative_ids)].export_parquet('./spectra/Structural_Modification/qtof_negative.parquet')
+    
+    # Save Similarities to utils
+    positive_sim.to_csv('./util/Structural_Modification/qtof_positive_sim.csv', index=False)
+    negative_sim.to_csv('./util/Structural_Modification/qtof_negative_sim.csv', index=False)
+    
+
 def main():
-    subsets = ['Bruker_Fragmentation_Prediction','MH_MNA_Translation','Orbitrap_Fragmentation_Prediction','Thermo_Bruker_Translation','GNPS_default']
+    subsets = ['Bruker_Fragmentation_Prediction','MH_MNA_Translation','Orbitrap_Fragmentation_Prediction',\
+                'Thermo_Bruker_Translation','Structural_Modification','GNPS_default']
     parser = argparse.ArgumentParser(
                     prog = 'GNPS2 Subset Generator',
                     description = 'This program generates predetermined subsets splits from GNPS2.')
@@ -153,11 +229,14 @@ def main():
         Orbitrap_Fragmentation_Prediction(csv_path, parquet_path)
     elif args.subset == 'Thermo_Bruker_Translation':
         Thermo_Bruker_Translation(csv_path, parquet_path)
+    elif args.subset == 'Structural_Modification':
+        Structural_Modification(csv_path, parquet_path)
     elif args.subset == 'GNPS_default':
         Bruker_Fragmentation_Prediction(csv_path, parquet_path)
         MH_MNA_Translation(csv_path, parquet_path)
         Orbitrap_Fragmentation_Prediction(csv_path, parquet_path)
         Thermo_Bruker_Translation(csv_path, parquet_path)
+        Structural_Modification(csv_path, parquet_path)
         
             
 if __name__ == '__main__':
